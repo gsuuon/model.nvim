7cb526e1b54877a32741a4b4736fe1fe6f2ca29b
Steven Sun <gsuuon@gmail.com>
2023-05-23 18:47:48 -0500

    refactor: usable via py

--- pythonx/store.py
+++ pythonx/store.py
@@ -10,6 +10,7 @@ import openai
 import tiktoken
 
 from typing import TypedDict, Optional
+from typing_extensions import TypeGuard
 
 # TODO make token counting optional
 # TODO we probably just want to store the entire files in store.json instead of re-reading them
@@ -19,6 +20,7 @@ enc = tiktoken.encoding_for_model('gpt-4')
 
 # https://platform.openai.com/docs/api-reference/embeddings/create
 INPUT_TOKEN_LIMIT = 8192
+DEFAULT_STORE_PATH = 'store.json'
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -43,10 +45,14 @@ def normalize_filepath(filepath: str) -> str:
 class Item(TypedDict):
     id: str
     content_hash: str
+    content: str
     meta: Optional[dict] # NotRequired not supported
 
-class StoreItem(Item):
+class StoreItem(TypedDict):
+    id: str
+    content_hash: str
     embedder: str
+    meta: Optional[dict] # NotRequired not supported
 
 class Store(TypedDict):
     items: list[StoreItem]
@@ -97,7 +103,7 @@ def cache_content(content: str):
     content_cache[hash] = content
     return hash
 
-def try_inject_content(item: Item):
+def try_inject_content(item: StoreItem):
     if item['content_hash'] in content_cache:
         return { **item, 'content':content_cache[item['content_hash']] }
     else:
@@ -127,6 +133,7 @@ def ingest_files(root_dir, glob_pattern) -> list[Item]:
             try:
                 return {
                     'id': normalize_filepath(filepath),
+                    'content': content,
                     'content_hash': cache_content(content),
                     'meta': {
                         'type': 'file'
@@ -179,21 +186,24 @@ def get_removed_item_store_idx(items: list[Item], store: Store):
         if item['id'] not in current_ids
     ]
 
-def _update_embeddings(items: list[Item], store: Store, remove_missing, print_updating_items=True) -> bool:
+def update_embeddings(
+    items: list[Item],
+    store: Store,
+    sync
+) -> list[str]:
     """
-    Update stale store data returning True if items were updated. remove_missing removes any items in store that aren't in provided items.
-    For partial updates (only adding items), disable remove_missing.
+    Update stale store data returning updated item ids. sync=True removes any items in store that aren't in provided items.
+    For partial updates (only adding items), set sync=False.
     """
     needs_update_idx = get_stale_or_new_item_idxs(items, store)
+
+    if len(needs_update_idx) == 0:
+        eprint('all ' + str(len(items)) + ' were stale')
+        return []
+
     needs_update_content = [ items[idx]['content'] for idx in needs_update_idx ]
 
-    if print_updating_items:
-        eprint('Updating items:')
-        eprint([ items[idx]['id'] for idx in needs_update_idx ])
-
     embeddings = get_embeddings(needs_update_content)
-
-    if len(embeddings) == 0: return False
 
     if store['vectors'] is None:
         vector_dimensions = len(embeddings[0])
@@ -201,7 +211,7 @@ def _update_embeddings(items: list[Item], store: Store, remove_missing, print_up
 
     assert store['vectors'] is not None
 
-    if remove_missing:
+    if sync:
         idxs = get_removed_item_store_idx(items, store)
         for idx in idxs:
             del store['items'][idx]
@@ -224,19 +234,13 @@ def _update_embeddings(items: list[Item], store: Store, remove_missing, print_up
             store['items'].append(item)
             store['vectors'] = np.vstack((store['vectors'], embedding))
 
-    return True
-
-def add_embeddings(items: list[Item], store):
-    return _update_embeddings(items, store, remove_missing=False)
-
-def sync_embeddings(items: list[Item], store):
-    return _update_embeddings(items, store, remove_missing=True)
+    return [ items[idx]['id'] for idx in needs_update_idx ]
 
 class Query(TypedDict):
     prompt: str
     count: int
 
-def query_store(prompt: str, count: int, store: Store, filter=None):
+def _query_store(prompt: str, count: int, store: Store, filter=None):
     assert store['vectors'] is not None
 
     embedding = get_embeddings([prompt], print_token_counts=False)[0]
@@ -260,45 +264,78 @@ def query_store(prompt: str, count: int, store: Store, filter=None):
 
         return results
 
-class Opts(TypedDict):
+class UpdateStoreOpts(TypedDict):
+    sync: bool
+    store_path: Optional[str]
+    files_ingest_root: Optional[str]
+    files_ingest_glob: Optional[str]
+
+class QueryOpts(TypedDict):
     prompt: str
     count: Optional[int]
     store_path: str
-    files_ingest_root: Optional[str]
-    files_ingest_glob: Optional[str]
+
+Opts = QueryOpts | UpdateStoreOpts
+
+def is_query(opts: Opts) -> TypeGuard[QueryOpts]:
+    return 'prompt' in opts
+
+def is_update_store(opts: Opts) -> TypeGuard[UpdateStoreOpts]:
+    return 'sync' in opts
 
 def get_opts() -> Opts:
     if len(sys.argv) < 2:
         raise ValueError('Missing options json argument')
 
-    opts : Opts = json.loads(sys.argv[1])
+    def assert_query():
+        assert type(opts['prompt']) == str, 'Missing prompt'
+        assert opts.get('count') == None or type(opts['count']) == int, 'count not a number'
 
-    assert type(opts['prompt']) == str, 'Missing prompt'
-    assert (opts['count'] == None or type(opts['count']) == int), 'count not a number'
-    assert type(opts['store_path']) == str, 'Missing store_path'
-    assert type(opts['files_ingest_root']) == str, 'Missing files_ingest_root'
-    assert (opts['files_ingest_glob'] == None or type(opts['files_ingest_glob'] == str)), 'files_ingest_glob not a string'
+    def assert_update_store():
+        assert type(opts['sync'] == bool), 'Missing sync option'
+        assert opts.get('store_path') == None or type(opts['store_path']) == str, 'Missing store_path'
+        assert opts.get('files_ingest_root') == None or type(opts.get('files_ingest_root')) == str, 'Missing files_ingest_root'
+        assert opts.get('files_ingest_glob') == None or type(opts.get('files_ingest_glob') == str), 'files_ingest_glob not a string'
+
+    opts = json.loads(sys.argv[1])
+
+    if is_query(opts):
+        assert_query()
+    else:
+        assert_update_store()
 
     return opts
 
-opts = get_opts()
-store = load_or_initialize_store(opts['store_path'])
-updated = add_embeddings(
-    ingest_files(
-        opts['files_ingest_root'] or '.',
-        opts['files_ingest_glob'] or '**/*'
-    ),
-    store
-)
-if updated: save_store(store, opts['store_path'])
-
-results = list(map(
-    try_inject_content,
-    query_store(
-        opts['prompt'],
-        opts['count'] or 1,
-        store
+def update_store(updateOpts: UpdateStoreOpts, store: Store):
+    updated = update_embeddings(
+        ingest_files(
+            updateOpts.get('files_ingest_root') or '.',
+            updateOpts.get('files_ingest_glob') or '**/*'
+        ),
+        store,
+        updateOpts['sync']
     )
-))
 
-print(json.dumps(results))
+    if len(updated) > 0:
+        save_store(store, opts.get('store_path') or DEFAULT_STORE_PATH)
+
+    return updated
+
+def query_store(queryOpts: QueryOpts, store: Store):
+    return list(map(
+        try_inject_content,
+        _query_store(
+            queryOpts['prompt'],
+            queryOpts.get('count') or 1,
+            store
+        )
+    ))
+
+if __name__ == '__main__':
+    opts = get_opts()
+    store = load_or_initialize_store(opts['store_path'] or DEFAULT_STORE_PATH)
+
+    if is_update_store(opts):
+        print(json.dumps(update_store(opts, store)))
+    elif is_query(opts):
+        print(json.dumps(query_store(opts, store)))
