3b4681e1397cb1ba5c7d36804ee0a092bd76b71d
Steven Sun <gsuuon@gmail.com>
2023-06-01 22:32:41 -0500

    docs: docs for store.lua

--- lua/llm/store/store.lua
+++ lua/llm/store/store.lua
@@ -53,10 +53,6 @@ local function to_python(o)
   return [[json.loads(]] .. sanitized .. [[, strict=False)]]
 end
 
-function M.add_items(items)
-  vim.cmd([[py store.update_store_and_save(]] .. to_python(items) .. [[,s)]])
-end
-
 local ts_source = require('llm.store.sources.treesitter')
 
 ---@param function_item FunctionItem
@@ -69,10 +65,13 @@ local function normalize_function_item_filepath_to_store(function_item)
   }
 end
 
+-- Extracts lua functions as items
 local function to_lua_functions(file)
   return vim.tbl_map(normalize_function_item_filepath_to_store, ts_source.lang.lua.functions(file))
 end
 
+---@param glob string glob pattern to search for files, starting from current directory
+---@param to_items function converts each filepath to a list of items
 local function glob_to_items(glob, to_items)
   local filepaths = vim.fn.glob(glob,nil,true)
 
@@ -89,6 +88,10 @@ local function glob_to_items(glob, to_items)
   end
 
   return results
+end
+
+function M.add_items(items)
+  vim.cmd([[py store.update_store_and_save(]] .. to_python(items) .. [[,s)]])
 end
 
 return M
