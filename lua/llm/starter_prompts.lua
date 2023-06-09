469ed1523da0a4530411b31c2418df669e6004ea
Steven Sun <gsuuon@gmail.com>
2023-06-09 14:29:02 -0500

    feat: Add segment module to starter prompts and update code prompt with new model option

--- lua/llm/starter_prompts.lua
+++ lua/llm/starter_prompts.lua
@@ -1,6 +1,7 @@
 local llm = require('llm')
 local util = require('llm.util')
 local openai = require('llm.providers.openai')
+local segment = require('llm.segment')
 
 return {
   code = {
@@ -8,7 +9,8 @@ return {
     mode = llm.mode.INSERT_OR_REPLACE,
     params = {
       temperature = 0.2,
-      max_tokens = 1000
+      max_tokens = 1000,
+      model = 'gpt-3.5-turbo-0301'
     },
     builder = function(input, context)
       local surrounding_lines_count = 20
@@ -16,20 +18,37 @@ return {
       local text_before = util.string.join_lines(util.table.slice(context.before, -surrounding_lines_count))
       local text_after = util.string.join_lines(util.table.slice(context.after, 0, surrounding_lines_count))
 
-      local content = 'The text: ```' .. text_before .. '<@@>' .. text_after .. '```'
-
-      if #input > 0 then
-        content = content .. '\n\nExisting text at <@@>: ```' .. input .. '```'
-      end
-
-      return {
-        messages = {
-          {
-            role = 'user',
-            content = content .. '\n\nRespond with raw text to replace the token <@@> in the text. Do not include explanations. Do not include surrounding markers in the response.'
-          }
+      local messages = {
+        {
+          role = 'system',
+          content = 'Replace the token <@@> with valid code. Respond only with code, never respond with an explanation, never respond with a markdown code block containing the code.'
+        },
+        {
+          role = 'user',
+          content = 'The code:\n```\nfunction greet(name) { console.log("Hello " <@@>) }\n```\n\nExisting text at <@@>:\n```+ nme```\n'
+        },
+        {
+          role = 'assistant',
+          content = '+ name'
         }
       }
+
+      local content = 'The code:\n```\n' .. text_before .. '<@@>' .. text_after .. '\n```\n'
+
+      if #input > 0 then
+        content = content ..  '\n\nExisting text at <@@>:\n```' .. input .. '```\n'
+      end
+
+      if #context.args > 0 then
+        content = content .. context.args
+      end
+
+      table.insert(messages, {
+        role = 'user',
+        content = content
+      })
+
+      return { messages = messages }
     end
   },
   instruct = {
