43870616ff6cae0623e7b5cf92445e2cc3bc1e37
Steven Sun <gsuuon@gmail.com>
2023-08-30 14:21:54 -0500

    refactor: replace vim.system with uv
    resolves #5

--- lua/llm/curl.lua
+++ lua/llm/curl.lua
@@ -1,5 +1,5 @@
 local util = require('llm.util')
-local uv = vim.loop
+local system = require('llm.util.system')
 
 local M = {}
 
@@ -34,63 +34,19 @@ local function build_args(opts, stream)
   return args
 end
 
-
--- TODO eventually switch to using vim.system(), neovim 0.10 feature
 -- TODO -w "%{stderr}%{response_code}" to get status code from stderr
 ---@param opts { url : string, method : string, body : any, headers : {[string]: string} }
+---@param stream boolean
 ---@param on_stdout fun(text: string): nil
 ---@param on_error fun(text: string): nil
 local function run_curl(opts, stream, on_stdout, on_error)
-  local stdout = assert(uv.new_pipe(false), 'Failed to open stdout pipe')
-  local stderr = assert(uv.new_pipe(false), 'Failed to open stderr pipe')
-
-  local _error_output = ''
-
   local args = build_args(opts, stream)
 
   if M._is_debugging then
     util.show(args, 'curl args')
   end
 
-  local handle = assert(uv.spawn('curl',
-    {
-      args = args,
-      stdio = { nil, stdout, stderr }
-    },
-    function(exit_code, signal)
-      -- success
-      if exit_code == 0 then return end
-
-      -- sigint / cancelled
-      if exit_code == 1 and signal == 2 then return end
-
-      on_error(_error_output)
-    end
-  ), 'curl exited unexpectedly')
-
-  uv.read_start(stderr, function(err, text)
-    assert(not err, err)
-    if text then _error_output = _error_output .. text end
-  end)
-
-  uv.read_start(stdout, function(err, text)
-    assert(not err, err)
-
-    -- naked call to on_stdout that errors crashes nvim
-    local success, handler_err_trace = xpcall(
-      on_stdout,
-      function()
-        return debug.traceback('Error in curl on_stdout handler', 2)
-      end,
-      text
-    )
-
-    if not success then
-      error(handler_err_trace, 2)
-    end
-  end)
-
-  return function() handle:kill("sigint") end
+  return system(args, on_stdout, on_error)
 end
 
 ---@param opts { url : string, method : string, body : any, headers : {[string]: string} }
