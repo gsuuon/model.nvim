a0d4113a2a7b31c111dbd0a28f5cbd0417efda92
Steven Sun <gsuuon@gmail.com>
2023-11-11 17:29:46 -0500

    feat: starter chat gpt 3 and 4

--- lua/llm/prompts/chats.lua
+++ lua/llm/prompts/chats.lua
@@ -1,37 +1,52 @@
 local openai = require('llm.providers.openai')
 
----@type table<string, ChatPrompt>
-local chats = {
-  openai = {
-    provider = openai,
-    create = function(input, context)
-      return {
-        system = 'You are a helpful assistant',
-        config = {
-          model = 'gpt-3.5-turbo-1106'
-        },
-        messages = {
-          {
-            role = 'user',
-            content = input
-          }
+local chat_openai = {
+  provider = openai,
+  contents = {
+    system = 'You are a helpful assistant',
+    config = {
+      model = 'gpt-3.5-turbo-1106'
+    }
+  },
+  create = function(input, context)
+    return {
+      messages = {
+        {
+          role = 'user',
+          content = input
         }
       }
-    end,
-    run = function(contents)
-      local params = contents.config or {}
-      params.messages = contents.messages
+    }
+  end,
+  run = function(contents)
+    local params = contents.config or {}
+    params.messages = contents.messages
 
-      if contents.system then
-        table.insert(params.messages, 1, {
-          role = 'system',
-          content = contents.system
-        })
-      end
-
-      return { params = params }
+    if contents.system then
+      table.insert(params.messages, 1, {
+        role = 'system',
+        content = contents.system
+      })
     end
-  }
+
+    return { params = params }
+  end
+}
+
+---@type table<string, ChatPrompt>
+local chats = {
+  openai = chat_openai,
+  gpt4 = vim.tbl_deep_extend(
+    'force',
+    chat_openai,
+    {
+      contents = {
+        config = {
+          model = 'gpt-4-1106-preview',
+        }
+      }
+    }
+  )
 }
 
 return chats
