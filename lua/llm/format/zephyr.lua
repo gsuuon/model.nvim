ac87a8bb9b630dd58b66f5722f75604c70f4a5d2
Steven Sun <gsuuon@gmail.com>
2023-11-15 13:40:14 -0500

    refactor: zephyr format

--- lua/llm/format/zephyr.lua
+++ lua/llm/format/zephyr.lua
@@ -8,15 +8,16 @@ local M = {}
 
 ---Format LlmChatContents to a string list so they can be individually tokenized.
 ---reference: https://huggingface.co/HuggingFaceH4/zephyr-7b-beta
----@param contents LlmChatContents
----@param include_continue boolean Include the continue partial `\n<|assistant|>\n` as the last string. Avoid adding EOS to the last string for chat prompts.
+---@param messages LlmChatMessage[]
+---@param system string
+---@param include_continue? boolean Include the continue partial `\n<|assistant|>\n` as the last string. Avoid adding EOS to the last string for chat prompts.
 ---@return string[]
-function M.contents_to_strings(contents, include_continue)
-  local system = contents.system or 'You are a helpful assistant.'
+function M.contents_to_strings(messages, system, include_continue)
+  local result = {
+    '<|system|>\n' .. system
+  }
 
-  local result = {'<|system|>\n' .. system}
-
-  for _,msg in ipairs(contents.messages) do
+  for _,msg in ipairs(messages) do
     table.insert(result, '\n<|' ..  msg.role .. '|>\n' .. msg.content)
   end
 
@@ -43,14 +44,15 @@ local function tokenize(text, url_base, cb)
 end
 
 ---Tokenizes each message with LlamaCpp server separately then inserts an EOS (2) token between each item.
----@param contents LlmChatContents
+---@param messages LlmChatMessage[]
+---@param system string
 ---@param url_base string
 ---@param cb fun(tokens: number[]): any
-local function tokenize_messages(contents, url_base, cb)
+local function tokenize_messages(messages, system, url_base, cb)
   local BOS = 1
   local EOS = 2
 
-  local formatted_messages = M.contents_to_strings(contents, true)
+  local formatted_messages = M.contents_to_strings(messages, system, true)
 
   async(function(wait, resolve)
     local tokens_list = {
@@ -74,32 +76,27 @@ end
 
 ---Use as ChatPrompt.run in a zephyr ChatPrompt.
 ---Tokenizes each message individually and adds a 2 (EOS) token between messages.
-function M.chatprompt_run(contents)
-  return function(set_config)
+---@param messages LlmChatMessage[]
+---@param config table
+function M.chatprompt_run(messages, config)
+  return function(set_params)
     async(function(wait, resolve)
-      if contents.config.options.server.command then
-        wait(llamacpp.start_server(contents.config.options.server.command, resolve))
+      if config.options.server.command then
+        wait(llamacpp.start_server(config.options.server.command, resolve))
       end
 
       local tokens = wait(
         tokenize_messages(
-          contents,
-          contents.config.options.server.url or 'http://localhost:8080',
+          messages,
+          config.system or 'You are a helpful assistant',
+          config.options.server.url or 'http://localhost:8080',
           resolve
         )
       )
 
-      local config_with_prompt = vim.tbl_deep_extend(
-        'force',
-        contents.config,
-        {
-          params = {
-            prompt = tokens,
-          }
-        }
-      )
-
-      set_config(config_with_prompt)
+      set_params({
+        prompt = tokens
+      })
     end)
   end
 end
