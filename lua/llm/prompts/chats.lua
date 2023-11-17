5c77e274b87fdb4519bffc4f1f8f7c2a0053b4d4
Steven Sun <gsuuon@gmail.com>
2023-11-17 10:31:06 -0500

    refactor: update chat starters

--- lua/llm/prompts/chats.lua
+++ lua/llm/prompts/chats.lua
@@ -4,34 +4,22 @@ local llamacpp = require('llm.providers.llamacpp')
 
 local chat_openai = {
   provider = openai,
-  contents = {
-    system = 'You are a helpful assistant',
-    config = {
-      model = 'gpt-3.5-turbo-1106'
-    }
+  system = 'You are a helpful assistant',
+  params = {
+    model = 'gpt-3.5-turbo-1106'
   },
   create = function(input, context)
-    return {
-      messages = {
-        context.selection and {
-          role = 'user',
-          content = input
-        } or nil
-      }
-    }
+    return context.selection and input or ''
   end,
-  run = function(contents)
-    local params = contents.config or {}
-    params.messages = contents.messages
-
-    if contents.system then
-      table.insert(params.messages, 1, {
+  run = function(messages, config)
+    if config.system then
+      table.insert(messages, 1, {
         role = 'system',
-        content = contents.system
+        content = config.system
       })
     end
 
-    return { params = params }
+    return { messages = messages }
   end
 }
 
@@ -42,10 +30,8 @@ local chats = {
     'force',
     chat_openai,
     {
-      contents = {
-        config = {
-          model = 'gpt-4-1106-preview',
-        }
+      params = {
+        model = 'gpt-4-1106-preview',
       }
     }
   ),
