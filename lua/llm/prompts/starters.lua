c50d1e0a8db73ba0437acd7e46ea9f9a87f4bc98
Steven Sun <gsuuon@gmail.com>
2023-12-01 18:52:54 -0500

    feat: starter zephyr llamacpp example

--- lua/llm/prompts/starters.lua
+++ lua/llm/prompts/starters.lua
@@ -102,6 +102,27 @@ local starters = {
       }
     end
   },
+  zephyr = {
+    provider = llamacpp,
+    options = {
+      model = 'zephyr-7b-beta.Q5_K_M.gguf',
+      args = {
+        '-c', 8192,
+        '-ngl', 35
+      }
+    },
+    builder = function(input, context)
+      return {
+        prompt =
+          '<|system|>'
+          .. (context.args or 'You are a helpful assistant')
+          .. '\n</s>\n<|user|>\n'
+          .. input
+          .. '</s>\n<|assistant|>',
+        stops = {'</s>'}
+      }
+    end
+  },
   codellama = codellama.default_prompt,
   ['hf starcoder'] = {
     provider = huggingface,
