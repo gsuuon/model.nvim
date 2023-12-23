fefa09e5e136e6db439455972bf1ec9f3bbf3d5d
Steven Sun <gsuuon@gmail.com>
2023-12-23 11:50:32 -0600

    refactor: openai provider uses sse_client

--- lua/model/providers/openai.lua
+++ lua/model/providers/openai.lua
@@ -34,6 +34,7 @@ local function extract_chat_data(item)
   end
 end
 
+---@deprecated Completion endpoints are pretty outdated
 local function extract_completion_data(item)
   local data = util.json.decode(item)
   if data ~= nil and data.choices ~= nil then
@@ -48,75 +49,65 @@ end
 ---@param params? any Additional options for OpenAI endpoint
 ---@param options? { url?: string, endpoint?: string, authorization?: string } Request endpoint and url. Defaults to 'https://api.openai.com/v1/' and 'chat/completions'. `authorization` overrides the request auth header. If url is provided the environment key will not be sent, you'll need to provide an authorization.
 function M.request_completion(handlers, params, options)
-  local _all_content = ''
   options = options or {}
-
-  local endpoint = options.endpoint or 'chat/completions'
-  local extract_data = endpoint == 'chat/completions' and extract_chat_data or extract_completion_data
-
-  -- TODO should handlers being optional be a choice at the provider level or always optional for all providers?
-  local _handlers = vim.tbl_extend("force", {
-    on_partial = util.noop,
-    on_finish = util.noop,
-    on_error = util.noop,
-  }, handlers)
-
-  local handle_raw = provider_util.iter_sse_data(function(item)
-    local data = extract_data(item)
-
-    if data ~= nil then
-      if data.content ~= nil then
-        _all_content = _all_content .. data.content
-        _handlers.on_partial(data.content)
-      end
-
-      if data.finish_reason ~= nil then
-        _handlers.on_finish(_all_content, data.finish_reason)
-      end
-    else
-      local response = util.json.decode(item)
-
-      if response ~= nil then
-        _handlers.on_error(response, 'response')
-      else
-        if not item:match('%[DONE%]') then
-          _handlers.on_error(item, 'item')
-        end
-      end
-    end
-  end)
-
-  local function handle_error(error)
-    _handlers.on_error(error, 'curl')
-  end
-
-  local body = vim.tbl_deep_extend('force', default_params, params)
 
   local headers = { ['Content-Type'] = 'application/json' }
   if options.authorization then
     headers.Authorization = options.authorization
-  end
-
-  local url_ = options.url
-  if url_ then
-    -- ensure we have a trailing slash if url was provided by options
-    if not (url_:sub(-1) == '/') then
-      url_ = url_ .. '/'
-    end
-  else
-    -- default to OpenAI api
-    url_ = 'https://api.openai.com/v1/'
-
-    -- only check the OpenAI env key if options.url wasn't set
+  elseif not options.url then -- only check the OpenAI env key if options.url wasn't set
     headers.Authorization = 'Bearer ' .. util.env_memo('OPENAI_API_KEY')
   end
 
-  return curl.stream({
-    headers = headers,
-    method = 'POST',
-    url = url_ .. endpoint,
-    body = body,
-  }, handle_raw, handle_error)
+  local endpoint = options.endpoint or 'chat/completions' -- TODO does this make compat harder?
+  local extract_data = endpoint == 'chat/completions' and extract_chat_data or extract_completion_data
+
+  local completion = ''
+
+  local sse = provider_util.sse_client({
+    on_message = function(message, pending)
+      local data = extract_data(message.data)
+
+      if data ~= nil then
+        if data.content ~= nil then
+          completion = completion .. data.content
+          handlers.on_partial(data.content)
+        end
+
+        if data.finish_reason ~= nil then
+          handlers.on_finish(completion, data.finish_reason)
+        end
+      elseif not message.data:match('%[DONE%]') then
+        handlers.on_error(vim.inspect({
+          data = message.data,
+          pending = pending
+        }), 'Unrecognized SSE message data')
+      end
+    end,
+    on_other = function(content)
+      -- Non-SSE message likely means there was an error
+      handlers.on_error(content, 'OpenAI API error')
+    end,
+    on_error = handlers.on_error
+  })
+
+  return curl.stream(
+    {
+      headers = headers,
+      method = 'POST',
+      url = util.string.joinpath(
+        options.url or 'https://api.openai.com/v1/',
+        endpoint
+      ),
+      body = vim.tbl_deep_extend(
+        'force',
+        default_params,
+        params
+      ),
+    },
+    sse.on_stdout,
+    sse.on_error,
+    sse.on_exit
+  )
 end
 
 ---@param standard_prompt StandardPrompt
