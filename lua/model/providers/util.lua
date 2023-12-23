3122b08ae919317cad391602bf7f1e3f9a14ace1
Steven Sun <gsuuon@gmail.com>
2023-12-23 11:45:30 -0600

    feat: sse_client and tests

--- lua/model/providers/util.lua
+++ lua/model/providers/util.lua
@@ -52,12 +52,71 @@ function M.iter_sse_messages(fn)
   end
 end
 
+---@deprecated Use sse_client
 function M.iter_sse_data(fn)
   return M.iter_sse_messages(function(message)
     if message.data ~= nil then
       fn(message.data)
     end
   end)
+end
+
+---Handles Server-Sent Event messages as well as non-SSE responses
+---@class SseHandler
+---@field on_message fun(msg: {data:string, [string]:string }, pending: string): nil
+---@field on_other fun(out: string): nil
+---@field on_error fun(out: string): nil
+
+---Exposes handlers for curl output and translates for use with SSE handlers
+---@class SseClient
+---@field on_stdout fun(raw: string):nil
+---@field on_error fun(err: string, label?: string):nil
+---@field on_exit fun():nil
+
+---@param handlers SseHandler
+---@return SseClient client
+function M.sse_client(handlers)
+  local is_sse = false
+  local pending = ''
+
+  local function consume_sse_messages()
+    -- replaces the matched message with empty
+    pending = pending:gsub('(.-)\n\n', function(message)
+      handlers.on_message(parse_sse_message(message), pending)
+      return ''
+    end)
+  end
+
+  return {
+    on_stdout = function(raw)
+      -- each raw can contain:
+      --  partial of not an sse message
+      --  complete not sse message
+      --  partial sse message
+      --  complete sse message
+      --  complete multiple sse message
+      --  partial of multiple sse message
+
+      pending = pending .. raw
+
+      if is_sse then
+        consume_sse_messages()
+      elseif raw:match('^data:') then
+        is_sse = true
+        consume_sse_messages()
+      end
+    end,
+    on_error = handlers.on_error,
+    on_exit = function()
+      if #pending > 0 then
+        if is_sse then
+          handlers.on_message(parse_sse_message(pending), pending)
+        else
+          handlers.on_other(pending)
+        end
+      end
+    end
+  }
 end
 
 return M
---
+++ lua/tests/sse_spec.lua
@@ -0,0 +1,143 @@
+describe('server-sent events client', function()
+  local p_util = require('model.providers.util')
+
+  local sse_client = function()
+      local results = {}
+
+      local client = p_util.sse_client({
+        on_message = function(msg)
+          results.messages = results.messages or {}
+          table.insert(results.messages , msg)
+        end,
+        on_other = function(x)
+          results.others = results.others or {}
+          table.insert(results.others, x)
+        end,
+        on_error = function(err)
+          results.errors = results.errors or {}
+          table.insert(results.errors, err)
+        end,
+      })
+
+    return {
+      client = client,
+      results = results
+    }
+  end
+
+  describe('client', function()
+    it('calls on_message as messages arrive', function()
+      local sse = sse_client()
+
+      sse.client.on_stdout('data: boop\n')
+      assert.are.same({}, sse.results) -- no \n\n yet so we haven't gotten a full message
+
+      sse.client.on_stdout('data: beep\n\n')
+      assert.are.same({messages = {
+        {data='boop\nbeep'},
+      }}, sse.results)
+
+      sse.client.on_stdout('data: foo') -- \n\n not necessary for the last message
+      sse.client.on_exit()
+
+      assert.are.same({messages = {
+        {data='boop\nbeep'},
+        {data='foo'},
+      }}, sse.results)
+    end)
+
+    it('calls on_other only after on_exit', function()
+      local sse = sse_client()
+
+      sse.client.on_stdout('foo')
+      assert.are.same({}, sse.results)
+
+      sse.client.on_stdout('bar\n\n')
+      assert.are.same({}, sse.results)
+
+      sse.client.on_exit()
+
+      assert.are.same({others = {
+        'foobar\n\n'
+      }}, sse.results)
+    end)
+  end)
+
+  describe('client parses', function()
+    local client_parse_expect = function(expected, outputs)
+      local sse = sse_client()
+
+      for _,out in ipairs(outputs) do
+        sse.client.on_stdout(out)
+      end
+
+      sse.client.on_exit()
+
+      assert.are.same(expected, sse.results)
+    end
+
+    it('partial not sse', function()
+      client_parse_expect(
+        {
+          others = {'foobar'}
+        },
+        {
+          'foo',
+          'bar'
+        }
+      )
+    end)
+    it('complete not sse', function()
+      client_parse_expect(
+        {
+          others = {'foobar'}
+        },
+        {
+          'foobar'
+        }
+      )
+    end)
+    it('partial sse', function()
+      client_parse_expect(
+        {
+          messages = { {data = 'foobar'}}
+        },
+        {
+          'data: foo',
+          'bar'
+        }
+      )
+    end)
+    it('complete sse', function()
+      client_parse_expect(
+        {
+          messages = {{data = 'foobar'}}
+        },
+        {
+          'data: foobar'
+        }
+      )
+    end)
+    it('partial multiple sse', function()
+      client_parse_expect(
+        {
+          messages = {{data = 'foobar\nbaz'}}
+        },
+        {
+          'data: foo',
+          'bar\ndata: baz'
+        }
+      )
+    end)
+    it('complete multiple sse', function()
+      client_parse_expect(
+        {
+          messages = {{data ='foo\nbar'}}
+        },
+        {
+          'data: foo\ndata: bar'
+        }
+      )
+    end)
+  end)
+end)
