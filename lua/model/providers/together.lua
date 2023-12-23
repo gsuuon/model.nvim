2e2d16717eafaddd626f80f486598a039e97ee9d
Steven Sun <gsuuon@gmail.com>
2023-12-23 13:33:26 -0600

    refactor: together sse_client

--- lua/model/providers/together.lua
+++ lua/model/providers/together.lua
@@ -7,39 +7,38 @@ local p_util = require('model.providers.util')
 ---@type Provider
 local M = {
   request_completion = function (handler, params)
+    local sse = p_util.sse_client({
+      on_message = function (msg, pending)
+        local item = util.json.decode(msg.data)
+
+        if item and item.choices then
+          handler.on_partial(item.choices[1].text)
+        elseif msg.data == '[DONE]' then
+          handler.on_finish()
+        else
+          handler.on_error(pending, 'Unrecognized SSE response')
+        end
+      end,
+      on_error = handler.on_error,
+      on_other = handler.on_error
+    })
+
     return curl.stream(
       {
         url = 'https://api.together.xyz/inference',
         headers = {
           ['Authorization'] = 'Bearer ' .. util.env('TOGETHER_API_KEY'),
-          ['accept'] = 'application/json',
+          ['Accept'] = 'application/json',
           ['Content-Type'] = 'application/json'
         },
         body = vim.tbl_extend('force', params, {
           stream = true
         })
       },
-      p_util.iter_sse_data(function(data)
-        if data == '[DONE]' then
-          handler.on_finish()
-        else
-          local item = util.json.decode(data)
-
-          if item == nil then
-            return
-          end
-
-          if item.choices then
-            handler.on_partial(item.choices[1].text)
-            return
-          end
-
-          util.show(data, 'Unexpected data')
-        end
-      end),
-      function(err)
-        handler.on_error(vim.inspect(err), 'Together provider error')
-      end
+      sse.on_stdout,
+      sse.on_error,
+      sse.on_exit,
+      sse.on_headers
     )
   end
 }
